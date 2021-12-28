749ce9299a808b4f54c9b7ce58c6bb953c299ce4
AliSalman86 <eng.aliabdullah@gmail.com>
2021-12-27 21:54:58 -0600

    enhance the friends.py code

--- friends.py
+++ friends.py
@@ -4,21 +4,20 @@
 
 # hint: readlines()
 user_input = input("Please enter names of 3 nearby friends separated by spaces: ").lower().split()
-origin_read_file = open("people.txt", "r")
-file_content = origin_read_file.readlines()
-origin_read_file.close()
-file_content_mod = []
+people = open("people.txt", "r")
+nearby_people = [line.strip().lower() for line in people.readlines()]
+people.close()
 
-for line in file_content:
-    file_content_mod.append(line.rstrip("\n").lower())
-
-print(file_content_mod)
+print(nearby_people)
 
 for friend in user_input:
-    dest_writing_file = open("nearby_friends.txt", "r")
-    file_original_content = dest_writing_file.read()
-    dest_writing_file.close()
-    if friend in file_content_mod:
+    
+    if friend in nearby_people:
+        # below 3 lines are to copy the exist content and append it to the file when use write() to avoid override the existing content
+        dest_writing_file = open("nearby_friends.txt", "r")
+        file_original_content = dest_writing_file.read()
+        dest_writing_file.close()
+        # append the new content to the old content and write it to the new file
         print(f"{friend}, is in the list of people and been added to the nearby friends list!")
         file_writing = open("nearby_friends.txt", "w")
         file_writing.write(file_original_content + friend.title() + "\n")
